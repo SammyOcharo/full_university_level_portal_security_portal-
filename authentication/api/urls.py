735cb165566a353f3b28c716e582d0df7bb97464
sammy ocharo <obanyisammy@gmail.com>
2023-08-07 08:05:56 +0300

    add login api

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('security-login/', views.SecurityLoginAPIView.as_view(), name='security-login')
+    path('security-login/', views.SecurityLoginAPIView.as_view(), name='security-login'),
+    path('security-login-approve/', views.SecurityLoginApproveAPIView.as_view(), name='security-login-approve'),
 ]
