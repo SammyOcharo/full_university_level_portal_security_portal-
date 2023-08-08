814f5dc9ad7b8b7462f3d23d41cae66488e37cb7
sammy ocharo <obanyisammy@gmail.com>
2023-08-08 09:19:36 +0300

    add required models

---
+++ authentication/choices.py
@@ -0,0 +1,8 @@
+USER_ROLES = (
+    ('admin', "admin"),
+    ('accounts', 'accounts'),
+    ('students', 'students'),
+    ('schools', 'schools'),
+    ('library', 'library'),
+    ('it_admin', 'it_admin')
+)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .choices import USER_ROLES
+
+class Roles(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    short_name = models.CharField(choices=USER_ROLES, default='', max_length=20, unique=True, blank=False)
+    is_active = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.short_name
+    
+    class Meta:
+        verbose_name_plural = 'roles'
+        db_table = 'roles'
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=15)
+    id_number = models.CharField(max_length=9, unique=True, blank=False)
+    username = models.EmailField(unique=True, null=False)
+    role = models.ForeignKey(Roles, on_delete=models.DO_NOTHING, related_name='user_student_role', null=True)
+    status = models.IntegerField(default=0)
+    full_name = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'user'
+        
+
+    def __str__(self) -> str:
+        return self.email
+    
+
+class LoginOtp(models.Model):
+    email = models.EmailField()
+    otp = models.IntegerField()
+    is_validated = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'security_activation_otp'
+
+    def __str__(self):
+        return self.email
+    
+class PasswordResetOtp(models.Model):
+    email = models.EmailField()
+    otp = models.IntegerField()
+    is_validated = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'security_password_reset_otp'
+
+    def __str__(self):
+        return self.email
+    
