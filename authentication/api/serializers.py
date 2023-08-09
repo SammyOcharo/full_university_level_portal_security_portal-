b4a381fd3b7262ca7f9c85c200e3f043d8fccf6f
sammy ocharo <obanyisammy@gmail.com>
2023-08-09 10:01:26 +0300

    add email services

--- authentication/api/serializers.py
+++ authentication/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
 
 class SecurityLoginSerializer(serializers.Serializer):
-    pass
+    username = serializers.CharField()
+    password = serializers.CharField()
